229b0d3d01915aa7d73e2b00d66b8b5765cb620c
abraham241 <huguesmboundou@gmail.com>
2024-09-24 14:03:46 +0100

    sidebar

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,12 +1,12 @@
 import Aside from "../Components/Aside/Aside";
 
-const Layout = ({children}) => {
-    return (
-        <div className='grid grid-cols-[300px_1fr_300px]'>
-        <Aside/>
-        {children}
+const Layout = ({ children }) => {
+  return (
+    <div className="grid grid-cols-[1fr_300px_300px]">
+      {children}
+      <Aside />
     </div>
-    );
-}
+  );
+};
 
 export default Layout;
