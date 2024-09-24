cab733cfd3828989e3ab69606be2c8c0df2a44f0
abraham241 <huguesmboundou@gmail.com>
2024-09-24 09:56:11 +0100

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,17 @@
-import AllPosts from '../Components/Posts/AllPosts';
-import Layout from './Layout';
- 
+import AllPosts from "../Components/Posts/AllPosts";
+import Layout from "./Layout";
 
 const Home = () => {
   return (
     <Layout>
-      <main className="w-full flex flex-col justify-center p-5">
-            <AllPosts/>           
-        </main>
+      <main className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
+        <section className="w-full max-w-4xl bg-white shadow-md rounded-lg p-6">
+          <h1 className="text-3xl font-semibold text-center mb-6">
+            Latest Posts
+          </h1>
+          <AllPosts />
+        </section>
+      </main>
     </Layout>
   );
 };
